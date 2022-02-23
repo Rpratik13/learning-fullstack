54a13c21b73e4522d3799a11abe84dea7e731166
Pratik Rajbhandari <r.pratik013@gmail.com>
2022-02-23 16:47:16 +0545

    Add google authentication strategy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("./services/passport");
+
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ app: "test" });
-});
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
