cc90fb88f0bdf62ff338e41a037017b04ee792f3
Pratik Rajbhandari <r.pratik013@gmail.com>
2022-03-01 16:58:57 +0545

    Add credit increment

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,5 +2,21 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = (app) => {
-  app.post("/api/stripe", (req, res) => {});
+  app.post("/api/stripe", async (req, res) => {
+    if (!req.user) return res.status(401).send({ error: "You must log in" });
+
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 credits",
+      source: req.body.id,
+    });
+
+    const { credits } = req.user;
+    req.user.credits = credits ? credits + 5 : 5;
+
+    const user = await req.user.save();
+
+    res.send(user);
+  });
 };
