58f6c4e2406e18fd6dd06c13bd7c2616458ae1af
Pratik Rajbhandari <r.pratik013@gmail.com>
2022-03-01 13:27:49 +0545

    Add fetch user action

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,5 +1,10 @@
-export default function (state = {}, action) {
+import { FETCH_USER } from "../actions/types";
+
+export default function authReducer(state = {}, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return { ...state, user: action.payload };
+
     default:
       return state;
   }
