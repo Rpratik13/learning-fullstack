20be5f060e271659e3556f23ccdcb5da2408605b
Pratik Rajbhandari <r.pratik013@gmail.com>
2022-03-01 14:03:39 +0545

    Add login logout function

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,7 +1,30 @@
+import { connect } from "react-redux";
 import React, { Component } from "react";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
+    console.log(this.props);
+
     return (
       <nav>
         <div className="nav-wrapper">
@@ -9,9 +32,7 @@ class Header extends Component {
             Emaily
           </a>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a href="/auth/google">Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +40,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,9 +1,9 @@
 import { FETCH_USER } from "../actions/types";
 
-export default function authReducer(state = {}, action) {
+export default function authReducer(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
-      return { ...state, user: action.payload };
+      return action.payload || false;
 
     default:
       return state;
